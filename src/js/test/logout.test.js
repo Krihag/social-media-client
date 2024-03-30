ea40cfbf9dbb9d9b6bda775188615ccd187b5102
Krihag <125972171+Krihag@users.noreply.github.com>
2024-03-30 21:04:18 +0100

    jest logout test

--- src/js/test/logout.test.js
+++ src/js/test/logout.test.js
@@ -12,11 +12,14 @@ global.localStorage = {
 
 describe("logoutUser", () => {
   it("Should remove token and profile from local storage", () => {
+    // Adds in profiles and tokens to local storage so that we can see if they are removed after logout
     storage.save("token", "test");
     storage.save("profile", "test");
     expect(localStorage.token).toEqual(JSON.stringify("test"));
     expect(localStorage.profile).toEqual(JSON.stringify("test"));
     logout();
+
+    // Checks if the token and profile are removed from local storage
     expect(localStorage.token).toBeUndefined();
     expect(localStorage.profile).toBeUndefined();
   });
